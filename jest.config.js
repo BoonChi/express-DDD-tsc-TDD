f1a2d110014a174ce526a6b04ff1fcaa0c52a743
boon chi <hum.boon.chi@gmail.com>
2022-02-16 10:13:18 +0100

    💚 add done() to end the process

--- jest.config.js
+++ jest.config.js
@@ -13,4 +13,5 @@ module.exports = {
   },
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  testRunner: 'jest-jasmine2',
 };
--- src/utility/test/setupFile.ts
+++ src/utility/test/setupFile.ts
@@ -4,15 +4,19 @@ import UserSeeder from '@users/infrastructure/database/user.seeder';
 /**
  * Connect to a new in-memory database before running any tests
  */
-beforeAll(async () => {
+beforeAll(async (done) => {
   await mongooseServer.connectMemoryServer();
   /**
    * Insert seeders
    */
   await UserSeeder.execute();
+  done();
 });
 
 /**
  * Remove and close the db and server.
  */
-afterAll(async () => await mongooseServer.disconnect());
+afterAll(async (done) => {
+  await mongooseServer.disconnect()
+  done();
+});
